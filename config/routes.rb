695cea1ea245aceb73e9d71ba8379aedf101163d
nao1092006 <nao1092006@gmail.com>
2020-12-26 19:12:36 +0900

    サーバーサイドの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
+# ルーティングを追加
 Rails.application.routes.draw do
+  devise_for :users
+  # 下の行は削除する
+  # get 'messages/index
+
   root "messages#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:edit, :update]
 end
